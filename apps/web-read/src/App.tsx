8d5c3995e5fcb92949a8b36ec28f5ca398c68a3a
Werner Junior <werner.junior@outlook.com>
2026-07-16 10:02:11 -0300

    feat(web-read): wrap app in Astryx AppShell and TopNav

--- apps/web-read/src/App.tsx
+++ apps/web-read/src/App.tsx
@@ -1,5 +1,21 @@
+import { AppShell } from "@astryxdesign/core/AppShell";
+import { TopNav } from "@astryxdesign/core/TopNav";
+import { TopNavHeading } from "@astryxdesign/core/TopNav";
 import WorkItemsListPage from "./pages/WorkItemsListPage";
 
 export default function App() {
-  return <WorkItemsListPage />;
+  return (
+    <AppShell
+      variant="elevated"
+      contentPadding={4}
+      topNav={
+        <TopNav
+          label="Main navigation"
+          heading={<TopNavHeading heading="Work Items" />}
+        />
+      }
+    >
+      <WorkItemsListPage />
+    </AppShell>
+  );
 }
