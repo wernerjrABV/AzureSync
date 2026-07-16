956c097443663b0086429e96f0563e3434d5e816
Werner Junior <werner.junior@outlook.com>
2026-07-16 10:29:41 -0300

    feat(web-read): add SideNav and conditional info Banner to AppShell

--- apps/web-read/src/App.tsx
+++ apps/web-read/src/App.tsx
@@ -1,7 +1,13 @@
 import { AppShell } from "@astryxdesign/core/AppShell";
 import { TopNav } from "@astryxdesign/core/TopNav";
 import { TopNavHeading } from "@astryxdesign/core/TopNav";
+import { SideNav } from "@astryxdesign/core/SideNav";
+import { SideNavHeading } from "@astryxdesign/core/SideNav";
+import { SideNavItem } from "@astryxdesign/core/SideNav";
+import { Banner } from "@astryxdesign/core/Banner";
 import WorkItemsListPage from "./pages/WorkItemsListPage";
+
+const INFO_MESSAGE = "";
 
 export default function App() {
   return (
@@ -13,6 +19,14 @@ export default function App() {
           label="Main navigation"
           heading={<TopNavHeading heading="Work Items" />}
         />
+      }
+      sideNav={
+        <SideNav header={<SideNavHeading heading="Work Items" />}>
+          <SideNavItem label="Work Items" isSelected />
+        </SideNav>
+      }
+      banner={
+        INFO_MESSAGE ? <Banner status="info" title={INFO_MESSAGE} /> : undefined
       }
     >
       <WorkItemsListPage />
